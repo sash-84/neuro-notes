b7d7c20334f48bb9fcde1771c159e28cdd9a19f9
sash-84 <s.katale844@gmail.com>
2025-04-28 18:41:32 +0530

    app.jsx modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,43 @@
+import {Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Login from "./components/Login"
+import NotesPage from "./pages/NotesPage"
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import { useEffect } from "react";
+import {jwtDecode} from "jwt-decode";
 
 function App() {
+
+  const {setIsLoggedIn, setUserEmail, setUserName, logout} = useAppContext();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+      if (decoded.exp < currentTime) {
+        logout();
+      } else {
+        setIsLoggedIn(true);
+        setUserEmail(localStorage.getItem("userEmail"));
+        setUserName(localStorage.getItem("userName"));
+      }
+    } else {
+      logout();
+    }
+  }, []);
+
   return (
-    <div>
+    <div className="relative">
       <Navbar />
-      <Login />
+      <Routes>
+         <Route path="/" element={<Hero />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/notes" element={<NotesPage />} />
+      </Routes>
+      {/* <Footer /> */}
     </div>
   )
 }
