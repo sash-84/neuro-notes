a5f213e451cb45098d641aa86b8f28df30eb7386
sash-84 <s.katale844@gmail.com>
2025-04-28 18:28:13 +0530

    added notes routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import noteRoutes from "./routes/noteRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use("/api/user",userRoutes);
+app.use("/api/notes",noteRoutes);
 
 app.listen(5000, () => {
     console.log(`Connected to the server on PORT ${PORT}`);
