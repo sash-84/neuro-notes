e8a7d78ef3074f8b82567727a09a8b8ac0067443
sash-84 <s.katale844@gmail.com>
2025-04-28 18:41:50 +0530

    main.jsx modified

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <BrowserRouter>
+  <AppContextProvider>
     <App />
+    <Toaster position="top-right"/>
+    </AppContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
